c455cfe55a50fb1fca7ec1547f480201fdc6f569
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-07-10 10:58:42 +0530

    tokengating init

--- server.mjs
+++ server.mjs
@@ -10,7 +10,7 @@ const wasm = readFileSync('./AOS.wasm')
 // Create an instance of an Express application
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
